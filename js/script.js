012f499eff1697c1d2834b2a6f6b2401242fe506
BuiDucDuy <duybuiduc1@gmail.com>
2022-12-11 21:08:07 +0700

    Hoan thanh perfect pixel

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@ $('.testimonials').slick({
             speed: 500,
             cssEase: 'linear'
 });
+
+$(".list-nav .list-nav-item").click(function () { 
+    $(".list-nav .selected").removeClass("selected")
+    $(this).addClass("selected");
+    var active = $(".list-nav .selected").attr("data")
+    switch(active){
+        case '1':
+            $(".listing .active").removeClass("active")
+            $(".listing .top-sellers").addClass("active");
+            break;
+        case '2':
+            $(".listing .active").removeClass("active")
+            $(".listing .featured").addClass("active");
+            break;
+        case '3':
+            $(".listing .active").removeClass("active")
+            $(".listing .most-reviews").addClass("active");
+            break;
+    }
+});
